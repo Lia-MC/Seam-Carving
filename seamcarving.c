f6d2fa58cd77e9df22b635bc2209a61b8e35015e
Lia-MC <lia.mc73700@gmail.com>
2025-03-29 09:24:18 -0400

    modified calc energy function. theoretically should run

--- seamcarving.c
+++ seamcarving.c
@@ -4,12 +4,19 @@
 
 void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
 
-    int rx = right minus left;
-    int gx = ;
-    int bx = ;
-    int ry = top minus bottom;
-    int gy = ;
-    int by = ;
+    int rx1;
+    int gx1;
+    int bx1;
+    int ry1;
+    int gy1;
+    int by1;
+
+    int rx2;
+    int gx2;
+    int bx2;
+    int ry2;
+    int gy2;
+    int by2;
 
     int xvals = im->width - 1; // make this 1 less than the i-th
     int yvals = im->height - 1; // make this 1 less than the i-th
@@ -17,26 +24,31 @@ void calc_energy(struct rgb_img *im, struct rgb_img **grad) {
     int ygrad = 0;
 
     double energy = 0;
+    uint8_t dualgradientenergy;
 
     for (int i = 1; i < xvals; i++) { // start at 1 and ends before last pixel so that we dont do edges
         for (int j = 1; j < yvals; j++) {
-            // DO SOME PSEUDOCODE PLANNING ABOUT HOW TO ACCESS EACH FOUR SIDES (UP DOWN LEFT RIGHT)
-            // AND HOW TO CALCULATE THE CHANGE LIKE WHAT DO YOU WANNA ACTUALLY DO
 
-            ry1, rx1 = get_pixel(im, j-1, i-1, 0); // check that its returning ry, rx not rx, ry (actually it might be...)
-            gy1, gx1 = get_pixel(im, j-1, i-1, 1);
-            gy1, bx1 = get_pixel(im, j-1, i-1, 2);
+            ry1, rx1 = get_pixel(im, j+1, i, 0) - get_pixel(im, j-1, i, 0); 
+            gy1, gx1 = get_pixel(im, j+1, i, 0) - get_pixel(im, j-1, i, 1);
+            gy1, bx1 = get_pixel(im, j+1, i, 0) - get_pixel(im, j-1, i, 2);
 
-            ry2, rx2 = get_pixel(im, j, i, 0); // check that its returning ry, rx not rx, ry (actually it might be...)
-            gy2, gx2 = get_pixel(im, j, i, 1);
-            gy2, bx2 = get_pixel(im, j, i, 2);
+            ry2, rx2 = get_pixel(im, j, i+1, 0) - get_pixel(im, j, i-1, 0); 
+            gy2, gx2 = get_pixel(im, j, i+1, 0) - get_pixel(im, j, i-1, 1);
+            gy2, bx2 = get_pixel(im, j, i+1, 0) - get_pixel(im, j, i-1, 2);
 
-            xgrad = (rx) * (rx) + (gx) * (gx) + (bx) * (bx);
-            ygrad = (ry) * (ry) + (gy) * (gy) + (by) * (by);
+            xgrad = ((ry1) * (ry1), (rx1) * (rx1)) + ((gy1) * (gy1), (gx1) * (gx1)) + ((by1) * (by1), (bx1) * (bx1));
+            ygrad = ((ry2) * (ry2), (rx2) * (rx2)) + ((gy2) * (gy2), (gx2) * (gx2)) + ((by2) * (by2), (bx2) * (bx2));
 
             energy = sqrt(xgrad * xgrad + ygrad * ygrad);
+
+            energy *= 0.1;
+
+            dualgradientenergy = (uint8_t) energy;
+
+            // For each pixel, set the r, g, and b channels to the same value 
+            // (the energy divided by 10 and cast to uint8_t).
+            set_pixel(*grad, j, i, dualgradientenergy, dualgradientenergy, dualgradientenergy);
         }
     }
-
-    *grad = ; // put smth in it
 }
